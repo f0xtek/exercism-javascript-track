4a2f2ea6bcf5f1630ea71dccbdc504c709ac99a6
Luke Anderson <landerson61@gmail.com>
2020-09-13 19:39:55 +0100

    Iteration2

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,5 +1,5 @@
 const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
-const getIndex = color => COLORS.indexOf(color);
+const getColorIndex = color => COLORS.indexOf(color);
 
-export const decodedValue = colors => Number(colors.slice(0,2).map(color => getIndex(color)).join(''));
+export const decodedValue = colors => Number(colors.slice(0,2).map(getColorIndex).join(''));
